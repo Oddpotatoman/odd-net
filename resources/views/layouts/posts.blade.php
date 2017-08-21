4b504e5948ee80df069adbd55c9e4d3507653dd6
Odd Andre <oddi160@gmail.com>
2017-08-21 16:55:04 +0200

    FixBlogIndex

--- resources/views/layouts/posts.blade.php
+++ resources/views/layouts/posts.blade.php
@@ -2,6 +2,6 @@
     <a href="/blog/{{$post->id}}"><h2 class="blog-post-title">{{$post->title}}</h2></a>
     <p class="blog-post-meta">{{$post->user->name}} on {{ $post->created_at }}</p>
     <p class="blog-content-post">
-       {{$post->body}}
+       {!! $post->body !!}
     </p>
 </div>
